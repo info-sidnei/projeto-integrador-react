663bc0933e1149260cd57a61d7f35885cbbcc1b4
info-sidnei <info.sidnei@gmail.com>
2023-09-16 20:31:53 -0300

    alterado cadastro e pesquisa

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,6 +1,5 @@
-import './HomePage.module.css';
+import './HomePage.css';
 import AuthService from '../../services/AuthService';
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuthContext } from '../../contexts/auth/AuthContext';
 
@@ -8,14 +7,21 @@ import { useAuthContext } from '../../contexts/auth/AuthContext';
 
 function HomePage() {
 
+  const home = () => { navigate('/home'); }
+
+  const read = () => { navigate('/read'); }
+
+  const create = () => { navigate('/create'); }
+
+  const update = () => { navigate('/update'); }
+
+  const deletar = () => { navigate('/delete'); }
+
   const { authService }: { authService: AuthService } = useAuthContext();
-  const [isLoggingOut, setIsLoggingOut] = useState(false);
   const navigate = useNavigate();
 
-  const logout = () => {
-    setIsLoggingOut(true);
+  const logout = () => { 
     authService.logout().then(() => {
-      setIsLoggingOut(false);
       navigate('/');
     })
   }
@@ -26,9 +32,17 @@ function HomePage() {
 
   return (
     <div>
-      <header>
-      <button className='clear' onClick={logout}>Sair</button>
-      </header>      
+      <div>
+      <div className='menu-container'>
+        <button className='clear' onClick={home}>Home</button>
+        <button className='clear' onClick={read}>Pesquisar</button>
+        <button className='clear' onClick={create}>Cadastrar Produto</button>
+        <button className='clear' onClick={update}>Update</button>
+        <button className='clear' onClick={deletar}>Deletar</button>
+        <button className='clear' onClick={logout}>Sair</button>
+      </div>
+
+      </div>      
       <main className='centralize'>      
         <h1>Bem vindo ao PharmaPlain!</h1>
         <div className='home'>
