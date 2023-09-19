edd174d9c05ebe56251ee360ab1e222a34899088
elcio-souza <elcio.souza@labora.tech>
2023-09-19 08:20:09 -0300

    Inclusão imagem, acessibilidade, mudança no menu,

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import logoFarmacia from '../img/pharmaplain1.png'
 
 import styles from "./Header.module.css"
 
 
 const Header = () => {
   return (
-        <header className={styles.header}>
-            <h1>PharmaPlain - Farmacologia Virtual</h1>
-        </header>
+    <header className={styles.header}>
+      <img src={logoFarmacia} alt="logo farmácia" />
+      <h1>PharmaPlain - Farmacologia Virtual</h1>
+      <img src={logoFarmacia} alt="logo farmácia" />
+    </header>
   )
 }
 
