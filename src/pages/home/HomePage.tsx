433e4256a1dafd9e8cb884596da582e19d6b484e
info-sidnei <info.sidnei@gmail.com>
2023-11-14 20:20:28 -0300

    asd

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,52 +1,24 @@
+import { HeaderUser } from '../../components/headeruser/HeaderUser';
 import './HomePage.css';
-import { useAuthContext } from '../../contexts/auth/AuthContext';
-import AuthService from '../../services/AuthService';
 import { useNavigate } from 'react-router-dom';
 import VLibras from '@djpfs/react-vlibras';
 
 function HomePage() {
 
-  const home = () => { navigate('/home'); }
-
-  const read = () => { navigate('/read'); }
-
-  const create = () => { navigate('/create'); }
-
-  const update = () => { navigate('/update'); }
-
-  const deletar = () => { navigate('/delete'); }
-
-  const { authService }: { authService: AuthService } = useAuthContext();
   const navigate = useNavigate();
-
-  const logout = () => { 
-    authService.logout().then(() => {
-      navigate('/');
-    })
-  }
 
   const Consult = () => {
     navigate('../read')
-  }  
+  }
 
   return (
     <div>
-      <div>
-      <div className='menu-container'>
-        <button className='clear' onClick={home}>Home</button>
-        <button className='clear' onClick={read}>Pesquisar</button>
-        <button className='clear' onClick={create}>Cadastrar Produto</button>
-        <button className='clear' onClick={update}>Update</button>
-        <button className='clear' onClick={deletar}>Deletar</button>
-        <button className='clear' onClick={logout}>Sair</button>
-      </div>
-
-      </div>      
-      <main className='centralize'>      
+      <HeaderUser />
+      <main className='centralize'>
         <h1>Bem vindo ao PharmaPlain!</h1>
         <div><h2>Adquira aqui informações farmacêuticas!</h2></div>
         <div className='home'>
-          <article>            
+          <article>
             <p>
               Tudo que você procura sobre indicação, laboratório, preço médio e
               outras curiosidades a respeito de qualquer medicamento disponível
@@ -59,7 +31,7 @@ function HomePage() {
         </div>
       </main>
       <div>
-      <VLibras />
+        <VLibras />
       </div>
     </div>
   )
